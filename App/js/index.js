b92cee6cbec8a97214276c9339a149a29fc1c9ed
yahatsuki <kabas3d@gmail.com>
2021-06-06 18:21:57 +0200

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,37 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Image")
+                .setHost(host,"xui_ui_image5")
+                .setLeft("1.5238095238095237em")
+                .setTop("1.5238095238095237em")
+                .setSrc("{xui.ini.img_pic}")
+            );
+            
+            append(
+                xui.create("xui.UI.Label")
+                .setHost(host,"xui_ui_label7")
+                .setLeft("8.380952380952381em")
+                .setTop("2.2857142857142856em")
+                .setWidth("22.17142857142857em")
+                .setHeight("3.5047619047619047em")
+                .setCaption("Label")
+            );
+            
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input5")
+                .setLeft("20.571428571428573em")
+                .setTop("17.523809523809526em")
+                .setWidth("18em")
+                .setHeight("10em")
+                .setLabelSize("8em")
+                .setLabelCaption("Text Area")
+                .setMultiLines(true)
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
